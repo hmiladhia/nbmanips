cce0dd2c0ec605e2e1904ba89d1d15f06480d4cf
Dhia Hmila <dhiahmila@gmail.com>
2021-05-06 20:13:55 +0200

    initial commit

---
+++ nbmanips/__init__.py
@@ -0,0 +1,33 @@
+import copy
+import json
+
+
+def read_ipynb(notebook_path):
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def write_ipynb(notebook, notebook_path):
+    with open(notebook_path, 'w', encoding='utf-8') as f:
+        json.dump(notebook, f)
+
+
+class Notebook:
+    def __init__(self, content):
+        self.nb = copy.deepcopy(content)
+
+    def __add__(self, other):
+        # Copying the notebook
+        nb = copy.copy(self.nb)
+
+        # Concatenating the notebooks
+        nb['cells'] = nb['cells'] + copy.copy(other.nb['cells'])
+        return Notebook(nb)
+
+    def to_ipynb(self, path):
+        write_ipynb(self.nb, path)
+
+    @classmethod
+    def read_ipynb(cls, path):
+        nb = read_ipynb(path)
+        return Notebook(nb)
