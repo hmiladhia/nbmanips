d23485ac72be3eb113a737b46a2f4a4ce9cee7d4
Dhia Hmila <dhiahmila@gmail.com>
2021-05-17 22:56:38 +0200

    added VERSION

--- nbmanips/__init__.py
+++ nbmanips/__init__.py
@@ -4,3 +4,5 @@ from nbmanips.selector import Selector
 from nbmanips.notebook_mixins import NotebookBase, SlideShowMixin
 from nbmanips.notebook import Notebook
 
+with open("nbmanips/VERSION", "r", encoding="utf-8") as fh:
+    __version__ = fh.read()
