99f119e94ff25661fe346cfc5a846c435e8d4102
Dhia Hmila <dhiahmila@gmail.com>
2021-05-18 01:11:23 +0200

    intermediary commit

--- nbmanips/notebook.py
+++ nbmanips/notebook.py
@@ -9,20 +9,20 @@ class Notebook(SlideShowMixin, NotebookBase):
         for cell in sel.iter_cells(self.nb['cells']):
             cell.set_source([line.replace(old, new) for line in cell.get_source(text=False)])
 
-    def tag(self, tag_key, tag_value, selector):
-        sel = Selector(selector)
+    def tag(self, tag_key, tag_value, selector, *args, **kwargs):
+        sel = Selector(selector, *args, **kwargs)
         for cell in sel.iter_cells(self.nb['cells']):
             cell.cell['metadata'][tag_key] = tag_value
 
-    def erase(self, selector):
-        sel = Selector(selector)
+    def erase(self, selector, *args, **kwargs):
+        sel = Selector(selector, *args, **kwargs)
         for cell in sel.iter_cells(self.nb['cells']):
             cell.set_source([])
 
-    def delete(self, selector):
-        self.nb['cells'] = [cell.cell for cell in Selector(selector, neg=True).iter_cells(self.nb['cells'])]
+    def delete(self, selector, *args, **kwargs):
+        self.nb['cells'] = [cell.cell for cell in ~Selector(selector, *args, **kwargs).iter_cells(self.nb['cells'])]
 
-    def keep(self, selector):
+    def keep(self, selector, *args, **kwargs):
         self.nb['cells'] = [cell.cell for cell in Selector(selector).iter_cells(self.nb['cells'])]
 
     def search(self, text, case=False):
--- nbmanips/selector.py
+++ nbmanips/selector.py
@@ -1,3 +1,4 @@
+from copy import copy
 from functools import partial
 
 from nbmanips import Cell
@@ -6,31 +7,46 @@ from nbmanips import Cell
 class Selector:
     default_selectors = {None: lambda cell: True}
 
-    def __init__(self, selector=None, neg=False):
-        self.neg = neg
-        self.is_slice = False
+    def __init__(self, selector, *args, **kwargs):
+        self.slice = False
+        self.list = False
+
         if callable(selector):
-            self._selector = selector
+            assert not args  # TODO: add message
+            self._selector = partial(selector, **kwargs)
+        elif isinstance(selector, int):
+            self.slice = slice(selector, *args, **kwargs)
         elif isinstance(selector, str):
-            self._selector = self.default_selectors[selector]
-        elif isinstance(selector, tuple):
-            self._selector = partial(self.default_selectors[selector[0]], **selector[1])
+            assert not args  # TODO: add message
+            self._selector = partial(self.default_selectors[selector], **kwargs)
+        elif isinstance(selector, list):
+            if len(args) == len(selector):
+                kwargs_list = args
+            else:
+                kwargs_list = args[0] if args else [{} for _ in selector]
+            self.list = [Selector(sel, **kwgs) for sel, kwgs in zip(selector, kwargs_list)]
+            # TODO: implement this
+            raise NotImplemented("This feature is not implemented yet")
         elif isinstance(selector, slice):
-            self.is_slice = True
-            self._selector = selector
+            self.slice = selector
+            start, stop, step = selector.start, selector.stop, selector.step
+            self._selector = lambda cell: (cell.num < stop) and ((cell.num-start) % step == 0)
 
     def select(self, cell):
-        result = self._selector(cell)
-        return not result if self.neg else result
+        return self._selector(cell)
+        # return not result if self.neg else result
 
-    def iter_cells(self, cells):
-        if self.is_slice:
-            return (Cell(cell, i) for i, cell in enumerate(cells[self._selector]))
+    def iter_cells(self, cells, neg=False):
+        # TODO: move to select
+        if self.slice:
+            if neg:
+                start, stop, step = self.slice.start, self.slice.stop, self.slice.step
+                return filter(lambda i: i >= stop or ((i-start) % step), (Cell(c, i) for i, c in enumerate(cells)))
+            else:
+                return (Cell(cell, i) for i, cell in enumerate(cells[self.slice]))
         else:
-            for i, cell in enumerate(cells):
-                cell = Cell(cell, i)
-                if self.select(cell):
-                    yield cell
+            return filter(lambda cell: not self.select(cell) if neg else self.select(cell),
+                          (Cell(cell, i) for i, cell in enumerate(cells)))
 
     @classmethod
     def register_selector(cls, key, selector):
@@ -61,5 +77,5 @@ def is_raw(cell):
 Selector.register_selector('contains', contains)
 Selector.register_selector('has_type', has_type)
 Selector.register_selector('raw_cells', is_raw)
-Selector.register_selector('markdown_cels', is_markdown)
+Selector.register_selector('markdown_cells', is_markdown)
 Selector.register_selector('code_cells', is_code)
