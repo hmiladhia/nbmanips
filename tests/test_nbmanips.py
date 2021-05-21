139b6d9d9a4114653a0e189c3b826f48bb904af4
Dhia Hmila <dhiahmila@gmail.com>
2021-05-22 00:21:14 +0200

    added tests for selector args

--- tests/test_nbmanips.py
+++ tests/test_nbmanips.py
@@ -119,13 +119,22 @@ def test_keep(nb0, selector, selector_kwargs, search_term, expected, expected_le
     (0, False), (1, False), (2, True), (3, True), (4, True),
     (5, False), (6, True), (7, True), (8, False),
 ])
-def test_has_readable_output(nb3, cell_num, expected):
-    from nbmanips.selector import has_readable_output
+def test_has_output(nb3, cell_num, expected):
+    from nbmanips.selector import has_output
     from nbmanips import Cell
 
     cell = Cell(nb3.cells[cell_num], cell_num)
-    assert has_readable_output(cell) == expected
+    assert has_output(cell) == expected
 
+
+@pytest.mark.parametrize("selector,args,expected", [
+    ('has_output', (), 1),
+    ('contains', ('Hello',), 1),
+    ('contains', ('hello', False), 1),  # case=False
+    ('contains', ('hello', True), None),  # case=True
+])
+def test_selector_args(nb1, selector, args, expected):
+    assert nb1.find(selector, *args) == expected
 
 # def test_selectors(nb0, selector, selector_kwargs):
 #     assert False
