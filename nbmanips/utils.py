2b1262c83ace366570846354fc734eb78f31fcfb
Dhia Hmila <dhiahmila@gmail.com>
2021-05-22 19:44:02 +0200

    added new styles

--- nbmanips/utils.py
+++ nbmanips/utils.py
@@ -18,24 +18,33 @@ def write_ipynb(notebook, notebook_path):
         json.dump(notebook, f)
 
 
-def printable_cell(text, style='unicode'):
-    width = shutil.get_terminal_size().columns - 1
+styles = {"single": "││┌─┐└─┘",
+          "double": "║║╔═╗╚═╝",
+          "grid": '||+-++-+',
+          "separated": '||+=++=+',
+          "rounded": "||.-.'-'",
+          "dots": "::......",
+          "simple": '  ======',
+          }
+
+
+def parse_style(style):
+    l, r, ul, u, ur, dl, d, dr = style
+
+    return l, r, ul, u, ur, dl, d, dr
+
+
+def printable_cell(text, width=None, style='single'):
+    width = width or (shutil.get_terminal_size().columns - 1)
+    style_l, style_r, style_ul, style_u, style_ur, style_dl, style_d, style_dr = parse_style(styles[style])
+
+    text_width = width - 2 - len(style_l) - len(style_r)
+
     result_list = []
     for text_line in text.split('\n'):
-        result_list.extend(wrap(text_line, width-4, tabsize=4))
+        result_list.extend(wrap(text_line, text_width, tabsize=4))
 
-    char = '║' if style == 'unicode' else '|'
-
-    if style == 'unicode':
-        result = '╔' + '═' * (width - 2) + '╗'
-    else:
-        result = '+' + '_' * (width-2) + '+'
-
-    result += '\n'
-    result += '\n'.join([f"{char} {line.ljust(width-4)} {char}" for line in result_list])
-    result += '\n'
-    if style == 'unicode':
-        result += '╚' + '═' * (width - 2) + '╝'
-    else:
-        result += '+' + '_' * (width - 2) + '+'
-    return result
+    result = [style_ul + style_u * (width - len(style_ul) - len(style_ur)) + style_ur]
+    result.extend([f"{style_l} {line.ljust(text_width)} {style_r}" for line in result_list])
+    result.append(style_dl + style_d * (width - len(style_dl) - len(style_dr)) + style_dr)
+    return '\n'.join(result)
