b2a540932665cd7826c4bbdba1aee400c83f574a
Dhia Hmila <dhiahmila@gmail.com>
2021-09-09 15:47:15 +0200

    added invert option

--- nbmanips/__main__.py
+++ nbmanips/__main__.py
@@ -323,17 +323,22 @@ def slides(
 @click.argument('selector', required=True)
 @click.argument('arguments', nargs=-1, required=False)
 @click.option('--kwarg', 'kwargs', multiple=True, type=(str, str))
-@click.option('--or', 'or_', is_flag=True)
-def select(selector, arguments, kwargs, or_):
+@click.option('--or', 'or_', is_flag=True, default=False)
+@click.option('--invert', '-i', is_flag=True, default=False)
+def select(selector, arguments, kwargs, or_, invert):
     if selector.isdigit():
         selector = int(selector)
     elif selector.replace(':', '').isdigit():
         selector = slice(*[int(p) for p in selector.split(':')])
 
     sel = Selector(selector, *arguments, **dict(kwargs))
+    if invert:
+        sel = ~sel
+
     piped_selector = get_selector()
     if piped_selector is not None:
         sel = (piped_selector | sel) if or_ else (piped_selector & sel)
+
     click.echo(cloudpickle.dumps(sel))
 
 
