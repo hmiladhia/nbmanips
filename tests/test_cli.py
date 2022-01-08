d2bbe135e83a2b48341f46a0de09396f77cdb494
Dhia Hmila <dhiahmila@gmail.com>
2022-01-08 21:24:00 +0100

    added explore cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -80,3 +80,54 @@ def test_to_md(runner):
         result = runner.invoke(cli, ['convert', 'md', 'nb.ipynb', '-o', 'exported.md'])
         assert result.exit_code == 0
         assert Path('exported.md').exists()
+
+
+def test_show(runner):
+    result = runner.invoke(cli, ['show', 'test_files/nb1.ipynb'])
+
+    assert result.exit_code == 0
+    assert len(result.output.strip().split('\n')) >= 3
+
+
+def test_count(runner):
+    result = runner.invoke(cli, ['count', 'test_files/nb1.ipynb'])
+
+    assert result.exit_code == 0
+    assert int(result.output.strip()) == 4
+
+
+def test_first(runner):
+    selection_result = runner.invoke(cli, ['select', 'is_empty'])
+    assert selection_result.exit_code == 0
+
+    result = runner.invoke(cli, ['first', 'test_files/nb3.ipynb'], input=selection_result.stdout_bytes)
+
+    assert result.exit_code == 0
+    assert int(result.output.strip()) == 5
+
+
+def test_last(runner):
+    selection_result = runner.invoke(cli, ['select', 'is_empty'])
+    assert selection_result.exit_code == 0
+
+    result = runner.invoke(cli, ['last', 'test_files/nb3.ipynb'], input=selection_result.stdout_bytes)
+
+    assert result.exit_code == 0
+    assert int(result.output.strip()) == 8
+
+
+def test_list(runner):
+    selection_result = runner.invoke(cli, ['select', 'is_empty'])
+    assert selection_result.exit_code == 0
+
+    result = runner.invoke(cli, ['list', 'test_files/nb3.ipynb'], input=selection_result.stdout_bytes)
+
+    assert result.exit_code == 0
+    assert result.output.strip() == '[5, 8]'
+
+
+def test_search(runner):
+    result = runner.invoke(cli, ['search', '-t', 'a', 'test_files/nb1.ipynb'])
+
+    assert result.exit_code == 0
+    assert result.output.strip() == '[0, 2, 3]'
