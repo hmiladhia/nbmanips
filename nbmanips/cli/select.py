2a6b0bf3df8037ce2c09a406bce3cbf58c433e80
Dhia Hmila <dhiahmila@gmail.com>
2022-01-09 13:08:06 +0100

    created SelectGroup

--- nbmanips/cli/select.py
+++ nbmanips/cli/select.py
@@ -1,7 +1,29 @@
 import click
 import cloudpickle
+from click import Group
 
 from nbmanips.selector import Selector
+
+__all__ = [
+    'select',
+    'get_selector'
+]
+
+
+class SelectGroup(Group):
+    def resolve_command(self, ctx, args):
+        cmd_name, cmd, args = super().resolve_command(ctx, args)
+        if cmd is select_unknown:
+            args = [cmd_name] + args
+        return cmd_name, cmd, args
+
+    def get_command(self, ctx, cmd_name):
+        return self.commands.get(cmd_name, select_unknown)
+
+    def list_commands(self, ctx):
+        commands = set(self.commands)
+        commands |= set(Selector.default_selectors)
+        return sorted(commands)
 
 
 def get_selector():
@@ -16,18 +38,39 @@ def get_selector():
     return cloudpickle.loads(stream)
 
 
+# https://click.palletsprojects.com/en/8.0.x/commands/
+
+
+@click.group(cls=SelectGroup)
+def select():
+    pass
+
+
 @click.command()
 @click.argument('selector', required=True)
 @click.argument('arguments', nargs=-1, required=False)
 @click.option('--kwarg', 'kwargs', multiple=True, type=(str, str))
 @click.option('--or', 'or_', is_flag=True, default=False)
 @click.option('--invert', '-i', is_flag=True, default=False)
-def select(selector, arguments, kwargs, or_, invert):
+def select_unknown(selector, arguments, kwargs, or_, invert):
     if selector.isdigit():
         selector = int(selector)
     elif selector.replace(':', '').isdigit():
         selector = slice(*[int(p) for p in selector.split(':')])
 
+    _select_unknown(selector, arguments, kwargs, or_, invert)
+
+
+@select.command(name='has_output_type', help='Select cells that have a given output_type')
+@click.argument('arguments', nargs=-1, required=False)
+@click.option('--kwarg', 'kwargs', multiple=True, type=(str, str))
+@click.option('--or', 'or_', is_flag=True, default=False)
+@click.option('--invert', '-i', is_flag=True, default=False)
+def has_output_type(arguments, kwargs, or_, invert):
+    _select_unknown('has_output_type', arguments, kwargs, or_, invert)
+
+
+def _select_unknown(selector, arguments, kwargs, or_, invert):
     sel = Selector(selector, *arguments, **dict(kwargs))
     if invert:
         sel = ~sel
