f3e5f2d6f31551015efeffbe5ac93f4a4fccbd9b
Dhia Hmila <dhiahmila@gmail.com>
2022-01-14 00:43:06 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().strip().split()
-
 with open("nbmanips/VERSION", "r", encoding="utf-8") as fh:
     version = fh.read().strip()
 
@@ -22,17 +19,19 @@ setuptools.setup(
     packages=['nbmanips', 'nbmanips.cli'],
     include_package_data=True,
     entry_points={
-        'console_scripts': ['nbmanips=nbmanips.__main__:nbmanips',
-                            'nb=nbmanips.__main__:nbmanips'],
+        'console_scripts': [
+            'nbmanips=nbmanips.__main__:nbmanips',
+            'nb=nbmanips.__main__:nbmanips'],
     },
-    install_requires=['nbconvert>=6.0.0',
-                      'nbformat>=5.1.3',
-                      'html2text==2020.1.16',
-                      'cloudpickle>=1.6.*',
-                      'click>=7.1.*',
-                      'Pygments>=2.10.*',
-                      'colorama>=0.4.*'
-                      ],
+    install_requires=[
+        'nbconvert>=6.0.0',
+        'nbformat>=5.1.3',
+        'html2text==2020.1.16',
+        'cloudpickle>=1.6.*',
+        'click>=7.1.*',
+        'Pygments>=2.10.*',
+        'colorama>=0.4.*'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
