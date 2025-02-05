c7f99d299da10e36d675c84cca3d3666b15b772b
Dhia Hmila <dhiahmila@gmail.com>
2025-02-05 11:26:17 +0100

    🔖 bump version

--- src/nbmanips/__init__.py
+++ src/nbmanips/__init__.py
@@ -1,5 +1,5 @@
 from nbmanips.notebook import DBC, IPYNB, ZPLN, Notebook
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 __author__ = "Dhia Hmila"
 __all__ = ["DBC", "IPYNB", "ZPLN", "Notebook", "__version__", "__author__"]
